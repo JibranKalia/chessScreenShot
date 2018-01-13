394fda8bc5c70db1b94e76edc760f9ba7b690ba6
Jibran Kalia <jibran.kalia@gmail.com>
2018-01-13 13:36:22 -0800

    Random vs Random working locally

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-// app.use(express.static('public'))
-app.get("/", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/public/index.html`));
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
